d3441f1e6d6502f9adf8d71ab30b17cee59a03d3
Leonardo Matos <leomp120894@gmail.com>
2019-03-26 12:39:33 -0300

    build: settup for now ci

---
+++ src/index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const { parse } = require('url')
+
+module.exports = (req, res) => {
+  const { query } = parse(req.url, true)
+  const { name = 'World' } = query
+  res.end(`Hello ${name}!`)
+}
