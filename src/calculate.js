47e104684af8772e0ae2f2f49683d3e4552a22ed
Talisson Ferreira <talissonf@gmail.com>
2019-03-27 13:13:38 -0300

    chore: debug post request

--- src/calculate.js
+++ src/calculate.js
@@ -17,6 +17,7 @@ module.exports = (request, response) => {
         .end({ status: 400, message: 'Not acceptable, body content must be a valid JSON with UTF-8 charset' })
     }
   })
+  response.send(body)
   //
   const { application, params } = body
   const frenetToken = application.hidden_data.frenet_access_token
