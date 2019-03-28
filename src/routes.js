d2543ffe2cfc17da6e4ddf80c18fabd8f826440b
Leonardo Matos <leomp120894@gmail.com>
2019-03-28 17:02:04 -0300

    feat: show package.json on domain root

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,12 @@
 'use strict'
+
 const router = require('express').Router()
+const pkg = require('../package.json')
 
 router.use('/callback', require('./callback'))
 router.use('/calculate', require('./calculate'))
+
+// show package.json on domain root
+router.get('/', (req, res) => res.send(pkg))
 
 module.exports = router
